39174ece9464a3b5baacbd27c02fa6074c817307
Roberto Orazi <robervgg@gmail.com>
2022-11-25 16:14:16 -0300

    first commit, adding add lead

--- xircls-frontend-react/src/router/routes/Customer.js
+++ xircls-frontend-react/src/router/routes/Customer.js
@@ -1,11 +1,15 @@
 import { lazy } from 'react'
 const Leads = lazy(() => import('../../views/Leads/Leads'))
-
+const Addlead = lazy(() => import('../../views/Leads/Add-lead'))
 
 const Customers_Routes = [
   {
     path: '/merchant/leads',
     element: <Leads />
+  },
+  {
+    path: '/merchant/customer/add-lead/',
+    element: <Addlead />
   }
 ]
 
