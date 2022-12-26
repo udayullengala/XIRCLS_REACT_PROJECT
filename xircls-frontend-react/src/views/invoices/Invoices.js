84d32830e483548661e6bf102ab6b4c057c02721
hanswatkins <hanswatkins@gmail.com>
2022-12-26 15:16:44 -0800

    used flex grid to create layout for statistics section

--- xircls-frontend-react/src/views/invoices/Invoices.js
+++ xircls-frontend-react/src/views/invoices/Invoices.js
@@ -18,39 +18,39 @@ const Invoices = () => {
                 </CardBody>
             </Card>
             <Card>
-                <CardBody style={{display: 'flex', justifyItems: 'space-between'}}>
-                        <img src={Cart}/>
-                    <div>
+                <CardBody style={{display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gridTemplateRows: 'repeat(2, 1fr)'}}>
+                    <div style={{gridArea: '1 / 1 / 2 / 2'}}className='div1'>
+                        <img style={{float: 'left', marginRight: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop:'10px'}} src={Cart}/>
                         <h6>0</h6>
                         <p>Orders Today</p>
                     </div>
-                        <img src={Box}/>
-                    <div>
+                    <div style={{gridArea: '1 / 2 / 2 / 3'}}className='div2'>
+                        <img style={{float: 'left', marginRight: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop:'10px'}} src={Box}/>
                         <h6>0.00</h6>
                         <p>Billing Today</p>
                     </div>
-                        <img src={Cart}/>
-                    <div>
+                    <div style={{gridArea: '1 / 3 / 2 / 4'}}className='div3'>
+                        <img style={{float: 'left', marginRight: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop:'10px'}} src={Cart}/>
                         <h6>28</h6>
                         <p>All Orders</p>
                     </div>
-                        <img src={Rupee}/>
-                    <div>
+                    <div style={{gridArea: '1 / 4 / 2 / 5'}}className='div4'>
+                    <img src={Rupee} style={{height: '30px', alignSelf: 'center', float: 'left', marginRight: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop:'10px'}} />
                         <h6>321407.00</h6>
                         <p>Total Billing</p>
                     </div>
-                        <img src={Box}/>
-                    <div>
+                    <div style={{gridArea: '2 / 1 / 3 / 2'}}className='div5'>
+                        <img style={{float: 'left', marginRight: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop:'10px'}} src={Box}/>
                         <h6>0.00</h6>
                         <p>Earnings Today</p>
                     </div>
-                        <img src={Cart}/>
-                    <div>
+                    <div style={{gridArea: '2 / 2 / 3 / 3'}}className='div6'>
+                        <img style={{float: 'left', marginRight: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop:'10px'}} src={Cart}/>
                         <h6>312778.00</h6>
                         <p>Total Earnings</p>
                     </div>
-                        <img src={Rupee}/>
-                    <div>
+                    <div style={{gridArea: '2 / 3 / 3 / 4'}}className='div7'>
+                        <img src={Rupee} style={{height: '30px', alignSelf: 'center', float: 'left', marginRight: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop:'10px'}}/>
                         <h6>8629.00</h6>
                         <p>Total Unpaid</p>
                     </div>
