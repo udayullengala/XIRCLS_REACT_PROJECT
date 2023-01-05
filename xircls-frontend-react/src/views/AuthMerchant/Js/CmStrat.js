d7a8beede0bfcde87e13cf935a58906c79cc932b
Roberto Orazi <robervgg@gmail.com>
2023-01-05 13:33:15 +0100

    responsive css updates

--- xircls-frontend-react/src/views/AuthMerchant/Js/CmStrat.js
+++ xircls-frontend-react/src/views/AuthMerchant/Js/CmStrat.js
@@ -17,12 +17,12 @@ const Cmstrat = () => {
                 <Row className="h-100 w-100 m-0 p-0">
                     <Col md={12} className="h-100" style={{ padding: '2rem' }}>
                         <Row>
-                            <h4 className="mb-2 fs-4 text-center" style={{ fontWeight: '600' }}>Select Your Cross-Marketing Strategy</h4>
+                            <h4 className="title mb-2 fs-4 text-center" style={{ fontWeight: '600' }}>Select Your Cross-Marketing Strategy</h4>
                         </Row>
                         <Row>
-                            <div id='selectcms' className="d-flex justify-content-evenly">
+                            <div id='selectcms' className="box d-flex justify-content-evenly">
                                 <div>
-                                    <Card id='cms1' onClick={() => cardSelect('cms1')} className="my-2 rounded-4 borderremover" style={{ width: '25rem', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', cursor: 'pointer', position: 'relative' }}>
+                                    <Card id='cms1' onClick={() => cardSelect('cms1')} className="item my-2 rounded-4 borderremover" style={{ width: '25rem', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', cursor: 'pointer', position: 'relative' }}>
                                         <CardBody className="px-3 text-center">
                                             <img className="mb-3" src='https://www.xircls.com/static/images/website-slide/Infiniti-logo.jpg' width='90' />
                                             <div className="mb-3">
@@ -36,7 +36,7 @@ const Cmstrat = () => {
                                     </Card>
                                 </div>
                                 <div>
-                                    <Card id='cms2' onClick={() => cardSelect('cms2')} className="my-2 rounded-4 borderremover" style={{ width: '25rem', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', cursor: 'pointer', position: 'relative' }}>
+                                    <Card id='cms2' onClick={() => cardSelect('cms2')} className="item my-2 rounded-4 borderremover" style={{ width: '25rem', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', cursor: 'pointer', position: 'relative' }}>
                                         <CardBody className="px-3 text-center">
                                             <img className="mb-3" src='https://www.xircls.com/static/images/website-slide/Sniper-logo.jpg' width='90' />
                                             <div className="mb-3">
@@ -49,7 +49,7 @@ const Cmstrat = () => {
                                     </Card>
                                 </div>
                                 <div>
-                                    <Card id='cms3' onClick={() => cardSelect('cms3')} className="my-2 rounded-4 borderremover" style={{ width: '25rem', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', cursor: 'pointer', position: 'relative' }}>
+                                    <Card id='cms3' onClick={() => cardSelect('cms3')} className="item my-2 rounded-4 borderremover" style={{ width: '25rem', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', cursor: 'pointer', position: 'relative' }}>
                                         <CardBody className="px-3 text-center">
                                             <img className="mb-3" src='https://www.xircls.com/static/images/website-slide/Infiniti-logo.jpg' width='90' />
                                             <div className="mb-3">
@@ -64,7 +64,7 @@ const Cmstrat = () => {
                             </div>
                         </Row>
                         <Row className="w-100 text-right px-1">
-                            <button className='btn waves waves-effect waves-dark text-white fs-5' style={{ width: '12.5%', marginLeft: 'auto', backgroundColor: '#19d895' }}>Proceed</button>
+                            <button className='btn btnp waves waves-effect waves-dark text-white fs-5' style={{ width: '12.5%', marginLeft: 'auto', backgroundColor: '#19d895' }}>Proceed</button>
                         </Row>
                     </Col>
                 </Row>
