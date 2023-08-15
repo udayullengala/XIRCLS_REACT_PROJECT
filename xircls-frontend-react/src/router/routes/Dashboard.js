0cdc4d452858a61cd05759f18a8148320e0f17be
hariharank-xircls <141998731+hariharank-xircls@users.noreply.github.com>
2023-08-15 12:45:25 +0530

    Update Dashboard.js

--- xircls-frontend-react/src/router/routes/Dashboard.js
+++ xircls-frontend-react/src/router/routes/Dashboard.js
@@ -1,5 +1,4 @@
 import { lazy } from 'react'
-import EmailSetup from '../../views/dashboard/EmailSetup'
 const Dashboard = lazy(() => import('../../views/dashboard/Dashboard'))
 
 
@@ -7,10 +6,6 @@ const Dashboard_Routes = [
   {
     path: '/merchant/dashboard',
     element: <Dashboard />
-  },
-  {
-    path: '/merchant/EmailSetup',
-    element: <EmailSetup />
   }
 ]
 
